3cb196884767852768e907f7e844a43cd2c7bbcb
= <=>
2017-06-30 23:25:25 -0400

    added middleware and filesystem module

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const fs = require('file-system');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 
 app.listen(3000,function(error){
